19ba755195cb69a8a0530a8c3e6fde57725bced3
Fran Papaolo <papaolofrann@gmail.com>
2026-08-24 19:41:51 -0300

    RED: el rover informa su posicion y orientacion iniciales

---
+++ 24Agosto/test_rover.py
@@ -0,0 +1,7 @@
+from rover import Rover
+
+def test_rover_informa_posicion_y_orientacion_iniciales():
+    rover = Rover(x=0, y=0, orientation="N")
+
+    assert rover.position() == (0, 0)
+    assert rover.orientation() == "N"
