4f3abed3320308b215f6be0aa6a71f5b6b4c9cd8
pecorarolucio <luciopecoraro2002@gmail.com>
2026-08-24 20:03:17 -0300

    REFACTOR: la orientacion pasa a ser un indice sobre una lista ciclica N-E-S-O

--- 24Agosto/rover.py
+++ 24Agosto/rover.py
@@ -1,18 +1,21 @@
 
 class Rover:
+
+    ORIENTATIONS = ["N", "E", "S", "O"]
+
     def __init__(self, x, y, orientation):
         self._x = x
         self._y = y
-        self._orientation = orientation
+        self._orientation_index = self.ORIENTATIONS.index(orientation)
 
     def position(self):
         return (self._x, self._y)
 
     def orientation(self):
-        return self._orientation
+        return self.ORIENTATIONS[self._orientation_index]
 
     def turn_right(self):
-        self._orientation = "E"
+        self._orientation_index = (self._orientation_index + 1) % 4
 
     def turn_left(self):
-        self._orientation = "O"
+        self._orientation_index = (self._orientation_index - 1) % 4
