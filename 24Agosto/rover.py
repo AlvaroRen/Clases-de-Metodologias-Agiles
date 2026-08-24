0916677d9ef015e776e00a5ec388bc695b2ebd56
pecorarolucio <luciopecoraro2002@gmail.com>
2026-08-24 20:17:05 -0300

    GREEN: el rover avanza una celda mirando al este

--- 24Agosto/rover.py
+++ 24Agosto/rover.py
@@ -3,11 +3,18 @@ class Rover:
 
     ORIENTATIONS = ["N", "E", "S", "O"]
 
+    _DELTAS = {
+        "N": (0, 1),
+        "E": (1, 0),
+        "S": (0, -1),
+        "O": (-1, 0),
+    }
+
     def __init__(self, x, y, orientation):
         self._x = x
         self._y = y
         self._orientation_index = self.ORIENTATIONS.index(orientation)
-
+    
     def position(self):
         return (self._x, self._y)
 
@@ -22,3 +29,8 @@ class Rover:
 
     def move_forward(self):
         self._y += 1
+
+    def move_forward(self):
+        dx, dy = self._DELTAS[self.orientation()]
+        self._x += dx
+        self._y += dy
