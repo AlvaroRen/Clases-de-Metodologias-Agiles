5cfd79018b8533014a54847eb3df529622ded8e6
Martín Mercado <martinmercado1502@gmail.com>
2026-08-24 19:49:00 -0300

    RED: el rover gira a la derecha

--- 24Agosto/test_rover.py
+++ 24Agosto/test_rover.py
@@ -6,3 +6,8 @@ def test_rover_informa_posicion_y_orientacion_iniciales():
     assert rover.position() == (0, 0)
     assert rover.orientation() == "N"
 
+def test_rover_gira_a_la_derecha():
+    rover = Rover(x=0, y=0, orientation="N")
+    rover.turn_right()
+
+    assert rover.orientation() == "E"
